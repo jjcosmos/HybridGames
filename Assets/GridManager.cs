03c8d4fb364452f9b0e174a068326d82a222bf2e
jjcosmos <32972128+jjcosmos@users.noreply.github.com>
2020-04-11 20:19:03 -0700

    setting up the gameboard

---
+++ Assets/GridManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+public class GridManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    List<GridSpace> myGridSpaces;
+    void Awake()
+    {
+        myGridSpaces = transform.GetComponentsInChildren<GridSpace>().ToList<GridSpace>();
+    }
+
+    public GridSpace GetGridSpaceAt(int x, int y)
+    {
+        foreach (var gridspace in myGridSpaces)
+        {
+            if(gridspace.x == x && gridspace.y == y)
+            {
+                return gridspace;
+            }
+        }
+        return null;
+    }
+}
---
+++ Assets/GridSpace.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSpace : MonoBehaviour
+{
+    public int x;
+    public int y;
+
+    [HideInInspector]
+    public Vector3 center;
+    static Vector3 offset;
+
+    public Vector3 GetCenter()
+    {
+        return transform.position + offset;
+    }
+}
