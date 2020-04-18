d04e94f7c4b16cec3a11bdd073c47e43003d4708
Michael Rapley <Michael.w.rapley@utah.edu>
2020-04-18 11:11:31 -0500

    Add scripts

---
+++ Assets/Scripts/Database/DatabasePlayerMoveInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+namespace database
+{
+    [Serializable]
+    public class DatabasePlayerMoveInfo
+    {
+        public int playerID = 0;
+        public string move = "";
+    }
+}
---
+++ Assets/Scripts/Database/Tester.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using database;
+
+public class Tester : MonoBehaviour
+{
+    public string gameID;
+    public DatabaseInteractionMobile mobile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    async void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            await mobile.EnterGameAsync(gameID);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            //mobile.SendTurnToDatabase(2);
+        }
+    }
+    
+    
+}
